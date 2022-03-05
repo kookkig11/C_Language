cf1ad4e1db46ad400563e7ffdae8183c236632d6
intrusive <kanittha.wo@ku.th>
2022-03-05 23:03:21 +0700

    Update lab10_5.cpp

--- lab10_5.cpp
+++ lab10_5.cpp
@@ -8,116 +8,106 @@ using namespace std;
 
 class Time
 {
-  public:
-    Time(int hour, int minute, int second) {
-      this->hour = hour;
-      this->minute = minute;
-      this->second = second;
-    }
-    
-    Time(int duration) {
-      this->hour = duration / 3600;
-      this->minute = (duration % 3600) / 60; 
-      this->second = (duration % 3600) % 60;
-    }
-    
-    int getHour() {
-      return hour;
-    }
-    
-    int getMinute() {
-      return minute;
-    }
-    
-    int getSecond() {
-      return second;
-    }
-    
-    int getDuration() const {
-      int this_time = 0;
-      this_time += this->hour * 60 * 60;
-      this_time += this->minute * 60;
-      this_time += this->second;
-      return this_time;
-    }
-    
-    Time add(Time other) const {
-      Time added_time(hour, minute, second);
-      
-      if (added_time.second + other.second >= 60) {
-        while (added_time.second + other.second >= 60) {
-          added_time.second = ((added_time.second + other.second) - 60);
-          other.second -= 60;
-          added_time.minute += 1;
+    private:
+        int hour;
+        int minute;
+        int second;
+
+    public:
+        // constructor
+        Time(int hour, int minute, int second) {
+            this -> hour = hour;
+            this -> minute = minute;
+            this -> second = second;
         }
-      } else {
-        added_time.second += other.second;
-      }
-      
-      if (added_time.minute + other.minute >= 60) {
-        while (added_time.minute + other.minute >= 60) {
-          added_time.minute = ((added_time.minute + other.minute) - 60);
-          other.minute -= 60;
-          added_time.hour += 1;
+
+        Time(int duration) {
+            // this -> second = duration;
+
+            int hour = duration / 3600;
+            duration -= hour * 3600;
+            int minute = duration / 60;
+            duration -= minute * 60;
+
+            this -> hour = hour;
+            this -> minute = minute;
+            this -> second = duration;
         }
-      } else {
-        added_time.minute += other.minute;
-      }
-      
-      added_time.hour += other.hour;
-      if (added_time.hour >= 24) {
-        added_time.hour -= 24;
-      }
-      return added_time;
-    }
-    
-    int subtract(Time other) const {
-      int this_time = 0, other_time = 0;
-      this_time += this->hour * 60 * 60;
-      this_time += this->minute * 60;
-      this_time += this->second;
-      
-      other_time += other.hour * 60 * 60;
-      other_time += other.minute * 60;
-      other_time += other.second;
-      
-      if (this_time >= other_time) {
-        return abs(this_time - other_time);
-      } else {
-        return 86400 - abs(this_time - other_time);
-      }
-    }
-    
-    int equals(Time other) const {
-      if (hour == other.hour && minute == other.minute && second == other.second) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-    
-    string toString() const {
-      int this_time = 0, hours, minutes, seconds;
-      this_time += this->hour * 60 * 60;
-      this_time += this->minute * 60;
-      this_time += this->second;
-      
-      hours = this_time / 3600;
-      minutes = (this_time % 3600) / 60; 
-      seconds = (this_time % 3600) % 60;
-      
-      if (hours >= 24) {
-        hours -= 24;
-      }
-      
-      ostringstream ss;
-      ss << setfill('0') << setw(2) << hours << ":" << setfill('0') << setw(2) << minutes << ":" << setfill('0') << setw(2) << seconds;
-      return ss.str();
-    }
-  
-  private:
-    int hour, minute, second, duration;
+
+        // setter
+        void setHour(int hour) {
+            this -> hour = hour;
+        }
+
+        void setMinute(int minute) {
+            this -> minute = minute;
+        }
+
+        void setSecond(int second) {
+            this -> second = second;
+        }
+
+        // getter
+        int getHour() { return hour; }
+        int getMinute() { return minute; }
+        int getSecond() { return second; }
+
+        // func.
+        int getDuration() {
+            int total_second = 0;
+            total_second += this->hour * 3600;
+            total_second += this->minute * 60;
+            total_second += this->second;
+            return total_second;
+        }
+
+        Time add(Time other) {
+            int total_second = 0;
+            total_second = this -> getDuration();
+            total_second += other.getHour() * 3600;
+            total_second += other.getMinute() * 60;
+            total_second += other.getSecond();
+
+            Time temp = Time(total_second);
+            int tHour = temp.getHour();
+            if (tHour >= 24) {
+                tHour -= 24;
+                temp.setHour(tHour);
+            }
+
+            return temp;
+        }
+
+        int subtract(Time other) {
+            int answer, sec1, sec2;
+            sec1 = this -> getDuration();
+            sec2 = other.getDuration();
+            if (sec1 < sec2) {
+                sec1 += 24*3600;
+            }
+            answer = sec1 - sec2;
+            return answer;
+        }
+
+        int equals(Time other) {
+            if (this->getDuration() == other.getDuration()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        string toString() {
+            stringstream timer;
+            if (this -> hour >= 24) {
+                this -> hour -= 24;
+            }
+
+            timer << setfill('0') << setw(2) << hour << ":" << setfill('0') << setw(2) << minute << ":" << setfill('0') << setw(2) << second;
+            return timer.str();
+        }
 };
+
 int main()
 {
    // implement program to test class Time
